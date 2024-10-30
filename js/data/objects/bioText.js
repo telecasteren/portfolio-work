cce835e3e3a248076615082b965636cdf54ddc35
telecasteren <127958662+telecasteren@users.noreply.github.com>
2024-10-30 16:53:11 +0100

    mobile screen

--- js/data/objects/bioText.js
+++ js/data/objects/bioText.js
@@ -1,44 +1,9 @@
-// export const bioText = {
-//   paragraphTitles: {
-//     title1: "Long story short,",
-//     title2: "Away from the screen,",
-//     title3: "My work,",
-//     title4: "Fun facts,",
-//   },
-
-//   paragraphText: {
-//     bulletpoint1:
-//       "Born in 1991 and raised on heavy metal and punk rock music. 🤘🏼",
-//     bulletpoint2:
-//       "Spending all my teenage allowance on concerts, records and instruments. 🎶",
-//     bulletpoint3:
-//       "Worked in the specialty coffee industry for approx. 14 years (ages 15.25 to 29.25).",
-//     bulletpoint4:
-//       "When it comes to food and drinks, I'll admit I'm a feinschmecker. Good quality to the people!",
-//     bulletpoint5:
-//       "I love being in hands-on creative- and learning environments.",
-//     bulletpoint6: "Working out. 🏋🏼‍♂️",
-//     bulletpoint7: "Traveling and exploring the world. 🌏",
-//     bulletpoint8:
-//       "Learn new stuff — although often leading me back to the screen. 🤓",
-//     bulletpoint9:
-//       "Inspired by nature, I try to bring natural elements into my work.",
-//     bulletpoint10:
-//       "The French say “Mise an place” — Everything in place. I'm a fan of structure.",
-//     bulletpoint11:
-//       "Achieving truly well directed interactive systems for humans is a passion of mine.",
-//     bulletpoint12:
-//       "I like keeping things simple, so my general approach is that “less is more”.",
-//   },
-// };
-// export default bioText;
-
 const bioText = [
   {
     title: "Long story short,",
     bullets: [
       "Born in 1991 and raised on heavy metal and punk rock music. 🤘🏼",
-      "Spending all my teenage allowance on concerts, records, and instruments. 🎶",
+      "Spent all my teenage allowance on concerts, records, and instruments. 🎶",
       "Worked in the specialty coffee industry for approx. 14 years (ages 15.25 to 29.25).",
       "When it comes to food and drinks, I'll admit I'm a feinschmecker. Good quality to the people!",
       "I love being in hands-on creative- and learning environments.",
@@ -55,10 +20,10 @@ const bioText = [
   {
     title: "My work,",
     bullets: [
-      "Inspired by nature, I try to bring natural elements into my work.",
-      "The French say “Mise an place” — Everything in place. I'm a fan of structure.",
+      "Inspired by nature, I try to bring natural elements into my work. 🍃",
+      "The French say “Mise an place” — Everything in place. I'm a fan of structure. 🧬",
       "Achieving truly well-directed interactive systems for humans is a passion of mine.",
-      "I like keeping things simple, so my general approach is that “less is more”.",
+      `I like keeping things simple, breaking down problems to bite size chunks. Like the late Ludwig Mies van der Rohe said, "Less is more".`,
     ],
   },
 ];
--- js/pages/bio/sections.js
+++ js/pages/bio/sections.js
@@ -1,73 +1,3 @@
-// import { mainSection, spinner } from "/js/data/constants.js";
-// import { alertMessage } from "/js/components/validation/messages.js";
-// import bioText from "../../data/objects/bioText.js";
-
-// export function displayBioText() {
-//   const pTitles = bioText.paragraphTitles;
-//   const pText = bioText.paragraphText;
-//   const pTitlesArray = Object.values(pTitles);
-//   const pTextArray = Object.values(pText);
-
-//   mainSection.classList.add("bioMainSection");
-
-//   let currentVisibleText = null;
-
-//   try {
-//     spinner.style.display = "none";
-
-//     pTitlesArray.forEach((title, index) => {
-//       const displayTextBtn = document.createElement("button");
-//       displayTextBtn.classList.add("textButton");
-//       displayTextBtn.innerText = title;
-
-//       const columnDiv = document.createElement("div");
-//       columnDiv.classList.add("column");
-
-//       const uList = document.createElement("ul");
-//       columnDiv.classList.add("uList");
-
-//       const listItem = document.createElement("li");
-//       listItem.classList.add("hiddenText", "listItem");
-//       listItem.innerHTML = pTextArray[index].replace(/\n/g, "<br /><br />");
-
-//       columnDiv.appendChild(listItem);
-//       columnDiv.appendChild(uList);
-//       columnDiv.appendChild(displayTextBtn);
-//       mainSection.appendChild(columnDiv);
-
-//       displayTextBtn.addEventListener("click", () => {
-//         if (currentVisibleText && currentVisibleText !== listItem) {
-//           currentVisibleText.classList.remove("visibleText");
-//           currentVisibleText.classList.add("hiddenText");
-//           currentVisibleText.style.maxHeight = "0px";
-//         }
-
-//         if (listItem.classList.contains("hiddenText")) {
-//           listItem.classList.remove("hiddenText");
-//           listItem.classList.add("visibleText");
-//           listItem.style.maxHeight = listItem.scrollHeight + "px";
-//           currentVisibleText = listItem;
-//         } else {
-//           listItem.classList.remove("visibleText");
-//           listItem.classList.add("hiddenText");
-//           listItem.style.maxHeight = "0px";
-//           currentVisibleText = null;
-//         }
-
-//         if (currentVisibleText) {
-//           columnDiv.scrollIntoView({
-//             behavior: "smooth",
-//             block: "start",
-//           });
-//         }
-//       });
-//     });
-//   } catch (error) {
-//     alertMessage("Couldn't load text content", "error");
-//     throw error;
-//   }
-// }
-
 import { mainSection, spinner } from "/js/data/constants.js";
 import { alertMessage } from "/js/components/validation/messages.js";
 import bioText from "../../data/objects/bioText.js";
@@ -132,33 +62,6 @@ export function displayBioText() {
         }
       });
     });
-
-    // displayTextBtn.addEventListener("click", () => {
-    //   if (currentVisibleText && currentVisibleText !== uList) {
-    //     currentVisibleText.classList.remove("visibleText");
-    //     currentVisibleText.classList.add("hiddenText");
-    //     currentVisibleText.style.maxHeight = "0px";
-    //   }
-
-    //   if (uList.classList.contains("hiddenText")) {
-    //     uList.classList.remove("hiddenText");
-    //     uList.classList.add("visibleText");
-    //     uList.style.maxHeight = uList.scrollHeight + "px";
-    //     currentVisibleText = uList;
-    //   } else {
-    //     uList.classList.remove("visibleText");
-    //     uList.classList.add("hiddenText");
-    //     uList.style.maxHeight = "0px";
-    //     currentVisibleText = null;
-    //   }
-
-    //   if (currentVisibleText) {
-    //     columnDiv.scrollIntoView({
-    //       behavior: "smooth",
-    //       block: "start",
-    //     });
-    //   }
-    // });
   } catch (error) {
     alertMessage("Couldn't load text content", "error");
     throw error;
